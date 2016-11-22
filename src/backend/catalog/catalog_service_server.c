c0a93e35206532ab43896a3c9b83fd6c78eee620
Kenan Yao <kyao@pivotal.io>
2016-11-22 22:18:36 +0800

    add a udf_cs_FindMyDatabase

---
+++ src/backend/catalog/catalog_service_server.c
@@ -0,0 +1,48 @@
+#include "postgres.h"
+
+Datum
+UDF_CS_FindMyDatabase(PG_FUNCTION_ARGS)
+{
+	char *dbname = PG_GETARG_CSTRING(0);
+	char *filename;
+	FILE *db_file;
+	Oid *this_id, *db_tablespace;
+	char thisname[NAMEDATALEN];
+	TransactionId db_frozenxid;
+
+	filename = database_getflatfilename();
+	db_file = AllocateFile(filename, "r");
+	if (db_file == NULL)
+		ereport(ERROR,
+				(errcode_for_file_access(),
+				errmsg("could not open file \"%s\": %m", filename)));
+
+	TupleDesc tupdesc = CreateTemplateTupleDesc(2, false);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 1, "db_id", OIDOID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 2, "db_tablespace", OIDOID, -1, 0);
+	tupdesc = BlessTupleDesc(tupdesc);
+	Assert(tupdesc != NULL);
+
+	Datum values[2];
+	char nulls[2];
+	HeapTuple tuple;
+
+	while (read_pg_database_line(db_file, thisname, db_id, db_tablespace, &db_frozenxid))
+	{
+		if (strcmp(thisname, name) == 0)
+		{
+			Assert(*db_id > 0 && *db_tablespace > 0);
+			values[0] = ObjectIdGetDatum(*oid);
+			nulls[0] = ' ';
+			values[1] = ObjectIdGetDatum(*db_tablespace);
+			nulls[1] = ' ';
+
+			tuple = heap_formtuple(tupdesc, values, nulls);
+			PG_RETURN_DATUM(HeapTupleGetDatum(tuple));
+		}
+	}
+
+	ereport(ERROR,
+			(errcode(ERRCODE_UNDEFINED_DATABASE),
+			 errmsg("database does not exist")));
+}
