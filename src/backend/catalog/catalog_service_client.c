179e53996c984d29cce034e96f1fa52c913562cc
Kenan Yao <kyao@pivotal.io>
2016-11-25 00:29:50 +0800

    Add FindMyDatabase libpq implementation; linking error caused by interfaces/libpq conflict with backend/libpq

--- src/backend/catalog/catalog_service_client.c
+++ src/backend/catalog/catalog_service_client.c
@@ -4,11 +4,16 @@
 #include "postgres.h"
 #include "libpq-fe.h"
 
+#define MAX_QUERY_LENGTH 1024
+#define MAX_DBNAME_LENGTH 64
+
+void FindMyDatabase_cs_cli(const char *name, Oid *db_id, Oid *db_tablespace);
+
 static bool connection_made = false;
 static PGconn *conn = NULL;
 
 static void
-cleanup_connection()
+teardown_connection()
 {
 	PQfinish(conn);
 }
@@ -29,41 +34,39 @@ initialize_connection()
 	/* Check to see that the backend connection was successfully made */
 	if (PQstatus(conn) != CONNECTION_OK)
 	{
-		cleanup_connection();
-		ereport(ERROR, "Connection to database failed");
+		teardown_connection();
+		elog(ERROR, "Connection to database failed");
 	}
 	connection_made = true;
 }
 
-bool
+void
 FindMyDatabase_cs_cli(const char *name, Oid *db_id, Oid *db_tablespace)
 {
 	initialize_connection();
 
-	char *query;
+	char query[MAX_QUERY_LENGTH];
 	char *errMsg;
 	PGresult *res;
-	int nFields, i;
 
-	if (strlen(name) > 200)
+	if (strlen(name) > MAX_DBNAME_LENGTH)
 		elog(ERROR, "database name too long");
-	if (sprintf(query, "SELECT * FROM CS_UDF_db_exist('%s')", name) < 0)
+	if (sprintf(query, "SELECT * FROM udf_cs_findmydatabase('%s') as (db_id oid, db_tablespace oid)", name) < 0)
 		elog(ERROR, "building query failed");
 	res = PQexec(conn, (const char *) query);
 	if (PQresultStatus(res) != PGRES_TUPLES_OK)
 	{
 		PQclear(res);
 		errMsg = pstrdup(PQerrorMessage(conn));
-		cleanup_connection();
+		teardown_connection();
 		elog(ERROR, "query failed: %s", errMsg);
 	}
 
-	if (PQnfields(res) != 1 || PQntuples(res) != 1)
+	if (PQnfields(res) != 2 || PQntuples(res) != 1)
 		elog(ERROR, "wrong results returned");
 
-	bool exist = strcmp(PQgetvalue(res, 0, 0), "t") == 0;
+	*db_id = (Oid) atoi(PQgetvalue(res, 0, 0));
+	*db_tablespace = (Oid) atoi(PQgetvalue(res, 0, 1));
 
 	PQclear(res);
-
-	return exist;
 }
